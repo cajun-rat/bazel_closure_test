247857d11aff1f448cde720992f645a2f3a894ca
Phil Wise <phil@phil-wise.com>
2018-08-14 20:27:17 +0200

    Use standard main() for test

--- server/hello_test.cc
+++ server/hello_test.cc
@@ -4,13 +4,3 @@
 TEST(Simple, simple) {
   EXPECT_EQ(1+1,2);
 }
-
-
-
-
-#ifndef __NO_MAIN__
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
-#endif
